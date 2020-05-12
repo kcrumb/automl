10b40f8eb4d159af409ecc002f62b21e081636f7
kcrumb <4051421+kcrumb@users.noreply.github.com>
2020-05-12 15:03:11 +0200

    creating a json on convertion

--- efficientdet/dataset/create_polyp_tfrecord.py
+++ efficientdet/dataset/create_polyp_tfrecord.py
@@ -47,11 +47,30 @@ polyp_label_map_dict = {
     'polyp': 0
 }
 
+GLOBAL_IMG_ID = 0  # global image id.
+GLOBAL_ANN_ID = 0  # global annotation id.
+
+
+def get_image_id(filename):
+    """Convert a string to a integer."""
+    del filename
+    global GLOBAL_IMG_ID
+    GLOBAL_IMG_ID += 1
+    return GLOBAL_IMG_ID
+
+
+def get_ann_id():
+    """Return unique annotation id across images."""
+    global GLOBAL_ANN_ID
+    GLOBAL_ANN_ID += 1
+    return GLOBAL_ANN_ID
+
 
 def dict_to_tf_example(img_path: str,
                        base_dir: str,
                        bboxes: list,
-                       label_map_dict
+                       label_map_dict,
+                       ann_json_dict=None
                        ):
     """Convert XML derived dict to tf.Example proto.
 
@@ -76,6 +95,17 @@ def dict_to_tf_example(img_path: str,
     width, height = image.size
     key = hashlib.sha256(encoded_img).hexdigest()
 
+    image_id = get_image_id(img_path)
+
+    if ann_json_dict:
+        image = {
+            'file_name': img_path,
+            'height': height,
+            'width': width,
+            'id': image_id,
+        }
+        ann_json_dict['images'].append(image)
+
     xmin = []
     ymin = []
     xmax = []
@@ -92,10 +122,30 @@ def dict_to_tf_example(img_path: str,
         classes_text.append(box[4].encode('utf8'))
         classes.append(label_map_dict[box[4]])
 
+        if ann_json_dict:
+            abs_xmin = int(box[0])
+            abs_ymin = int(box[1])
+            abs_xmax = int(box[2])
+            abs_ymax = int(box[3])
+            abs_width = abs_xmax - abs_xmin
+            abs_height = abs_ymax - abs_ymin
+            ann = {
+                'area': abs_width * abs_height,
+                'iscrowd': 0,
+                'image_id': image_id,
+                'bbox': [abs_xmin, abs_ymin, abs_width, abs_height],
+                'category_id': label_map_dict[box[4]],
+                'id': get_ann_id(),
+                'ignore': 0,
+                'segmentation': [],
+            }
+            ann_json_dict['annotations'].append(ann)
+
     example = tf.train.Example(features=tf.train.Features(feature={
         'image/height': tfrecord_util.int64_feature(height),
         'image/width': tfrecord_util.int64_feature(width),
         'image/filename': tfrecord_util.bytes_feature(img_path.encode('utf8')),
+        'image/source_id': tfrecord_util.bytes_feature(str(image_id).encode('utf8')),
         'image/key/sha256': tfrecord_util.bytes_feature(key.encode('utf8')),
         'image/encoded': tfrecord_util.bytes_feature(encoded_img),
         'image/object/bbox/xmin': tfrecord_util.float_list_feature(xmin),
@@ -145,16 +195,34 @@ def main(_):
     else:
         label_map_dict = polyp_label_map_dict
 
+    ann_json_dict = {
+        'images': [],
+        'type': 'instances',
+        'annotations': [],
+        'categories': []
+    }
+
+    for class_name, class_id in label_map_dict.items():
+      cls = {'supercategory': 'none', 'id': class_id, 'name': class_name}
+      ann_json_dict['categories'].append(cls)
+
     for idx, img in enumerate(img_anno):
         tf_example = dict_to_tf_example(img_path=img,
                                         base_dir=FLAGS.base_dir,
                                         bboxes=img_anno[img],
-                                        label_map_dict=label_map_dict
+                                        label_map_dict=label_map_dict,
+                                        ann_json_dict=ann_json_dict
                                         )
         writers[idx % FLAGS.num_shards].write(tf_example.SerializeToString())
 
     for writer in writers:
         writer.close()
+
+    json_file_path = os.path.join(
+        os.path.dirname(FLAGS.output_path),
+        'json_' + os.path.basename(FLAGS.output_path) + '.json')
+    with tf.io.gfile.GFile(json_file_path, 'w') as f:
+        json.dump(ann_json_dict, f)
 
 
 if __name__ == '__main__':
